1df22b63386333ddfbff1d9549d7d829408a8b61
dilmi214 <dilmifernando214@gmail.com>
2024-06-09 15:05:45 +0530

    Art gallery project first commit

---
+++ nova-gallery/src/main/java/io/github/dilmi214/artgallery/nova_gallery/artist/Artist.java
@@ -0,0 +1,13 @@
+package io.github.dilmi214.artgallery.nova_gallery.artist;
+
+import io.github.dilmi214.artgallery.nova_gallery.artpiece.Artpiece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Artist {
+    private Integer id;
+    private String name;
+    private String bio;
+    private List<Artpiece> artpieces;
+}
