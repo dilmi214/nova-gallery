814516879c30ebce42879391db909f64f63e564d
dilmi214 <dilmifernando214@gmail.com>
2024-06-21 23:13:29 +0530

    completed navbarand updated in the pages

--- nova-gallery/src/main/java/io/github/dilmi214/artgallery/nova_gallery/exhibition/ExhibitionController.java
+++ nova-gallery/src/main/java/io/github/dilmi214/artgallery/nova_gallery/exhibition/ExhibitionController.java
@@ -16,7 +16,7 @@ public class ExhibitionController {
         this.exhibitionService = exhibitionService;
     }
 
-    @GetMapping("/")
+    @GetMapping("")
     public String getAllExhibitions() {
         return "/Exhibition/exhibitions";
     }
